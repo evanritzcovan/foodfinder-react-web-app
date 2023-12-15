cd1fdd4f609b94b560fe8395766fff1faf9dfcb5
Evan Ritzcovan <evanritzcovan@gmail.com>
2023-12-15 16:45:25 -0500

    final

--- src/login/roles.js
+++ src/login/roles.js
@@ -3,6 +3,6 @@ export const Roles = {
   ADMIN: "ADMIN",
   // User can access everything except profile list
   USER: "USER",
-  // Guest can only access home page, login / register page, and about page
+  // Guest can only access home page, search, login / register page, and about page. Cannot bookmark restaurants.
   GUEST: "GUEST",
 };
